124f9a51127165b9b8bc1e06a332dff31f1ec5fb
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-09-11 00:54:01 -0300

    Update Linear_Regression.py

--- Linear Regression/Linear_Regression.py
+++ Linear Regression/Linear_Regression.py
@@ -6,20 +6,15 @@ from statsmodels import regression
 import statsmodels.api as sm
 import math
 
-pd.read_excel(r'')
+df = pd.read_excel('Assets PRBR11.xlsx', index_col = 'Data', parse_dates = True)
 
 def reg (X, Y):
     X = sm.add_constant(X)
     model = regression.linear_model.OLS(Y, X).fit()
-    a = model[0]
-    b = model[1]
-    X = X[:,1]
-
-    plt.styler.use('ggplot')
-    X2 = np.linspace(X.min(), X.max(), 100)
-    Y_hat = X2 * b + a
-    plt.scatter(X, Y, alpha=0.5)
-    plt.plot(X2, Y_hat, 'r', alpha=1.0)
-    plt.xlabel('X Value')
-    plt.ylabel('Y Value')
     return model.summary()
+
+def pct (stock):
+    st = df[stock].pct_change().dropna()
+    return st
+
+reg(pct('IBOV'), pct('POSI3'))
