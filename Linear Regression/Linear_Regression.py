df8320f85c13a3c73935343fd425f8fffa7b20d7
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-09-12 11:57:44 -0300

    Update Linear_Regression.py

--- Linear Regression/Linear_Regression.py
+++ Linear Regression/Linear_Regression.py
@@ -6,7 +6,7 @@ from statsmodels import regression
 import statsmodels.api as sm
 import math
 
-df = pd.read_excel('Assets PRBR11.xlsx', index_col = 'Data', parse_dates = True)
+df = pd.read_excel(r'Assets PRBR11.xlsx', index_col = 'Data', parse_dates = True)
 
 def reg (X, Y):
     X = sm.add_constant(X)
@@ -17,4 +17,11 @@ def pct (stock):
     st = df[stock].pct_change().dropna()
     return st
 
-reg(pct('IBOV'), pct('POSI3'))
+reg(pct('IBOV'), pct('CPLE6'))
+
+plt.style.use('ggplot')
+sns.regplot(data = df, 
+            x = pct('IBOV'),
+            y = pct('BTTL3'))
+plt.show()
+plt.clf()
